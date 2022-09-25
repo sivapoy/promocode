b0982886befd08b5811a7767aa8e5a31d28764fe
Sivabharathi Kumar <sivabharathi.k@outlook.com>
2022-09-25 11:54:27 +0530

    initial commit

---
+++ src/main/java/com/sivapoy/promocode/PoyPromoCodeApplication.java
@@ -0,0 +1,13 @@
+package com.sivapoy.promocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoyPromoCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoyPromoCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sivapoy/promocode/PoyPromoCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivapoy.promocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoyPromoCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
